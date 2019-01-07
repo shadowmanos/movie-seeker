eade2e0be618a2a378121d555871643b32d4933d
shadowmanos <shadowmanos@users.noreply.github.com>
2019-01-07 23:36:47 +0000

    set up project

---
+++ src/main/java/io/github/shadowmanos/movieseeker/MovieSeekerApplication.java
@@ -0,0 +1,14 @@
+package io.github.shadowmanos.movieseeker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieSeekerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieSeekerApplication.class, args);
+    }
+
+}
+
