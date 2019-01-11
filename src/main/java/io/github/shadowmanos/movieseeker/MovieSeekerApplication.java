6f8ae166c0c7f5faa30c17b2a843b45669ab4807
shadowmanos <shadowmanos@users.noreply.github.com>
2019-01-11 13:55:02 +0000

    format

--- src/main/java/io/github/shadowmanos/movieseeker/MovieSeekerApplication.java
+++ src/main/java/io/github/shadowmanos/movieseeker/MovieSeekerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MovieSeekerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MovieSeekerApplication.class, args);
     }
